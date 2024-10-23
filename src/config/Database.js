fe39d5f6572d6a0613d47c0549e38b563c21a7d0
Nimesh Tripathi <nimeshtripathi2004@gmail.com>
2024-10-23 13:16:50 +0530

    .

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,9 +1,3 @@
-const mongoose = require("mongoose")
 
-const connectDB = async () => {
-    await mongoose.connect("mongodb+srv://nimeshtripathi2004:iTKQGM250gdMXdVn@cluster0.8anuo.mongodb.net/devTinder")
-}
-
-module.exports = connectDB
 
 
